4c0cba57c8960dbc1aa8fa54ab843e5aa94479f7
Mark Lisher <lisher.mark@gmail.com>
2021-01-13 17:40:55 -0800

    Update Main Header to Expensify App

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import { startLogout } from '../actions/auth';
 
 export const Header = ({ startLogout }) => (
   <header>
-    <h1>Expensify</h1>
+    <h1>Expensify App</h1>
     <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
     <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
     <button onClick={startLogout}>Logout</button>

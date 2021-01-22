410f36074656bc4eb5aa17d11c5987789ca9dd05
Mark Lisher <lisher.mark@gmail.com>
2021-01-21 16:12:27 -0800

    Add styles to app

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import './firebase/firebase';
 import { firebase } from './firebase/firebase';
+import LoadingPage from './components/LoadingPage';
 
 const store = configureStore();
 
@@ -26,9 +27,9 @@ const renderApp = () => {
     ReactDOM.render(jsx, document.getElementById('app'));
     hasRendered = true
   }
-}
+};
 
-ReactDOM.render(<p>loading...</p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {

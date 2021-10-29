bb433ed350f919cf6c79176f0220f0fb7f02775f
Mark Lisher <lisher.mark@gmail.com>
2021-10-29 10:24:10 -0700

    Add alert message to login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,6 +6,7 @@ export const LoginPage = ({ startLogin }) => (
   <div className="box-layout">
     <div className="box-layout__box">
       <h1 className="box-layout__title">Expensify</h1>
+      <p> <span className="alert-heading">Important!</span> This app is just for testing. It may change or be removed at any time.</p>
       <p>It's time to get your expenses under control.</p>
       <button className="button" onClick={startLogin}>Login with Google</button>
     </div>    
